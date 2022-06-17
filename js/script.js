33cbb7e8da2b091726ec2fe829dc87a27cd489e8
Aleksey <savchits.aa@gmail.com>
2022-06-17 13:18:34 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let number = 4;
+const arr = ['a', 'b', 'c'];
+const arrObj = {
+    a:'a',
+    1:'b',
+    2:'c'
+};
+//коммит
+
+console.log(arr[1]);
+console.log(arrObj["1"]);
+
+//alert("что-то тут");
+//ввод пользователем со всплывающего окна
+//const result = confirm('ты тут?');
+//ответ на вопрос пользователя
+//const answer = prompt('Вам есть 18?','')
+//чтобы преобразовать из строки в число достаточно перед вырадением написать +
+//const answer = +prompt('Вам есть 18?','')
